b6aa16b8ac45ab1499e038fe90a4b60e016006ec
Aleksey <migitka.aleksey@gmail.com>
2026-04-19 18:40:53 +0300

    ref: изменил структуру кода и упростил его расширение

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,17 +28,25 @@ class AlienInvasion:
         """Запускает основной цикл игры."""
         while True:
             # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            self._check_events()
             # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._update_screen()
 
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Обрабатывает нажатие клавиш и события мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
