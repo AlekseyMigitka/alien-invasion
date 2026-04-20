cdd3da644e45f3e6e03b6427a14cf12814d085d2
Aleksey <migitka.aleksey@gmail.com>
2026-04-20 14:51:04 +0300

    ref: улучшил структуру кода и добавил флот пришельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,18 +88,29 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Создает флот пришельцев."""
-        # Создание пришельца и вычисление количества пришельцев в ряду.
-        # Интервал между соседними пришельцами равен ширине пришельца.
+        # Создание пришельца и добавление других, пока остается место.
+        # Расстояние между пришельцами составляет одну ширину
+        # и одну высоту пришельца.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            # Конец ряда: сбрасываем значение х и инкрементируем значение y.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """Создание пришельца и размещение его в ряду."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
