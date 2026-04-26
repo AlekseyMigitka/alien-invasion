c7c3c88ad9b7215188b4df470962bbb18ad0a4b6
Aleksey <migitka.aleksey@gmail.com>
2026-04-26 14:46:43 +0300

    Добавил постепенное повышение сложности

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,8 @@ class AlienInvasion:
         """Запускает новую игру при нажатии кнопки Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+            # Сброс игровых настроек.
+            self.settings.initialize_dynamic_settings()
             # Сброс игровой статистики.
             self.stats.reset_stats()
             self.game_active = True
@@ -135,6 +137,7 @@ class AlienInvasion:
             # Уничтожение существующих снарядов и создание нового флота.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_alien(self):
         """Обновление позиции всех пришельцев во флоте."""
--- settings.py
+++ settings.py
@@ -8,15 +8,27 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # Настройки корабля.
-        self.ship_speed = 5
         self.ship_limit = 3
         # Параметры снаряда.
-        self.bullet_speed = 3.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 10
         # Настройки пришельцев.
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+        # Темп ускорения игры.
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки изменяющиеся в ходе игры."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
